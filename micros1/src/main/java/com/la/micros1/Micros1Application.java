ac98d3adb5d20805999ba0d653d7f3d973e9574a
a.galilei <oC@hK5A9>
2021-10-11 21:25:28 +0400

    deploy app

---
+++ micros1/src/main/java/com/la/micros1/Micros1Application.java
@@ -0,0 +1,13 @@
+package com.la.micros1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Micros1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micros1Application.class, args);
+	}
+
+}
---
+++ micros1/src/test/java/com/la/micros1/Micros1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.la.micros1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Micros1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
